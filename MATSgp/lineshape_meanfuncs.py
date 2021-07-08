Gained 5 orders of magnitude in accuracy versus hapi pcqsdhc with small change in input to tf.math.polyval in hum1_wei. Also corrected reading data to take average temperature per dataset, fixing discontinuities in baseline.

MATSgp/lineshape_meanfuncs.py
@@ -215,7 +215,6 @@ def lineshape_from_dataframe(frame, limit_factor_dict={}, line_kwargs={}):
     n_dsets = len(nu_list)
     param_dict = {}
     vary_dict = {}
-    constraint_dict = {}
     vary_dict["nu"] = bool(
         np.sum(frame.filter(regex=r"nu_*\d_vary$").values)
     )  # Logical or, if any True, vary all
@@ -241,20 +240,27 @@ def lineshape_from_dataframe(frame, limit_factor_dict={}, line_kwargs={}):
                 vary_dict[new_name.replace("_vary", "")] = bool(val)
             else:
                 param_dict[new_name] = val
-                try:
-                    constraint_type, constraint_info = limit_factor_dict[name]
-                    if constraint_type == "magnitude":
-                        constraint_dict[new_name] = (
-                            val - constraint_info,
-                            val + constraint_info,
-                        )
-                    elif constraint_type == "factor":
-                        constraint_dict[new_name] = (
-                            val / constraint_info,
-                            val * constraint_info,
-                        )
-                except KeyError:
-                    pass
+    #Separately loop over parameters to get constraints
+    constraint_dict = {}
+    for name, val in frame.iteritems():
+        if ('vary' in name) or ("err" in name):
+            continue
+        else:
+            new_name = name.replace("_air", "").lower()
+            try:
+                constraint_type, constraint_info = limit_factor_dict[name]
+                if constraint_type == "magnitude":
+                    constraint_dict[new_name] = (
+                        val - constraint_info,
+                        val + constraint_info,
+                    )
+                elif constraint_type == "factor":
+                    constraint_dict[new_name] = (
+                        val / constraint_info,
+                        val * constraint_info,
+                    )
+            except KeyError:
+                pass
     # Create our lineshape function
     lineshape = LineShape(
         frame["molec_id"],

MATSgp/tf_pcqsdhc.py
@@ -5,33 +5,32 @@ fully compatible with tensorflow.
 import numpy as np
 import tensorflow as tf
 
+# define static data
+zone = np.complex128(1.0e0 + 0.0e0j)
+zi = np.complex128(0.0e0 + 1.0e0j)
+tt = np.float64(
+    [
+        0.5e0,
+        1.5e0,
+        2.5e0,
+        3.5e0,
+        4.5e0,
+        5.5e0,
+        6.5e0,
+        7.5e0,
+        8.5e0,
+        9.5e0,
+        10.5e0,
+        11.5e0,
+        12.5e0,
+        13.5e0,
+        14.5e0,
+    ]
+)
+pipwoeronehalf = np.float64(0.564189583547756e0)
 
 # ------------------ complex probability function -----------------------
 def cpf3(X, Y):
-    # define static data
-    zone = np.complex128(1.0e0 + 0.0e0j)
-    zi = np.complex128(0.0e0 + 1.0e0j)
-    tt = np.float64(
-        [
-            0.5e0,
-            1.5e0,
-            2.5e0,
-            3.5e0,
-            4.5e0,
-            5.5e0,
-            6.5e0,
-            7.5e0,
-            8.5e0,
-            9.5e0,
-            10.5e0,
-            11.5e0,
-            12.5e0,
-            13.5e0,
-            14.5e0,
-        ]
-    )
-    pipwoeronehalf = np.float64(0.564189583547756e0)
-
     zm1 = zone / tf.complex(X, Y)  # maybe redundant
     zm2 = zm1 ** 2
     zsum = zone
@@ -74,13 +73,8 @@ def cef(x, y, N):
     a = np.flipud(a[1 : N + 1])
     # Reorder coefficients.
     Z = (L + 1.0j * z) / (L - 1.0j * z)
-    p = tf.math.polyval([tf.cast(val, tf.complex128) for val in a.tolist()], Z)
+    p = tf.math.polyval([val+0.0j for val in a.tolist()], Z)
     # Polynomial evaluation.
-    # Not sure if above line will work with Tensorflow...
-    # Need to check because may fail quietly, i.e. break gradient without throwing error
-    # If can figure out what this whole function is supposed to be doing, though, may be able to just
-    # directly return w(z)... just need to know what zA2 and iz are...
-    # Hopefully using tf.math.polyval instead of np.polyval will do the trick.
     w = 2 * p / (L - 1.0j * z) ** 2 + (1 / np.sqrt(np.pi)) / (L - 1.0j * z)
     # Evaluate w(z).
     return w